059993a02c699b68a38265f38647f9be58934cc5
Brett Rudd <brettrudd@gmail.com>
2014-10-09 22:18:23 -0700

    Initial Commit

---
+++ android/3.0.0/src/com/phonegap/build/DefaultActivity.java
@@ -0,0 +1,42 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package com.phonegap.build;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class DefaultActivity extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        boolean hasSplash = false;
+        int splashScreenDuration = 5000;
+
+        super.onCreate(savedInstanceState);
+
+        if (hasSplash) {
+            super.loadUrl(Config.getStartUrl(), splashScreenDuration);
+        } else {
+            super.loadUrl(Config.getStartUrl());
+        }
+    }
+}
+
